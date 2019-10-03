a63944ec7b5a79048b97d7db8d1ee65c60804dd1
DianeDusenge <dianedusenge99@gmail.com>
2019-10-03 16:47:15 +0200

    THE changes I made on js file to make them valid

--- server/controllers/addentry.js
+++ server/controllers/addentry.js
@@ -1,3 +1,4 @@
+import validation from '../validate/validation';
 import entries from '../models/entryModel';
 const addentry = (req, res) => {
     let itemIds = entries.map(item => item.entryId);
@@ -5,23 +6,16 @@ const addentry = (req, res) => {
 
     let newId = itemIds.length > 0 ? Math.max.apply(Math, itemIds) + 1 : 1;
     // let newOrderNum = orderNums.length > 0 ? Math.max.apply(Math, orderNums) + 1 : 1;
-    if (!req.body.title) {
-        return res.status(400).send({
-            success: 'false',
-            descrition: 'title is required'
-        });
-    } else if (!req.body.description) {
-        return res.status(400).send({
-            success: 'false',
-            description: 'message is required'
-        });
-    }
 
+    const { error } = validation.validate(req.body);
+    if (error) {
+        return res.status(400).json({ status: 400, error: error.details[0].message });
+    }
     let newItem = {
         entryId: newId,
         title: req.body.title,
         description: req.body.description,
-        createdOn: new Date()
+        // createdOn: req.body.new Date(),
     };
 
     entries.push(newItem);
